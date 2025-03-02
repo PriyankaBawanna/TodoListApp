379dcabdf222ffde20cd886ba9761c6c9e21414f
Priyanka Bawanna <priyankabawanna@Priyankas-MacBook-Air.local>
2025-03-02 19:10:35 +0530

    Counter created

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 
-
 function Counter() {
- 
   const [count, setCount] = useState(0);
 
-
+  // Function to increment the count
   const increment = () => setCount(count + 1);
+
+  // Function to decrement the count
   const decrement = () => setCount(count - 1);
 
   return (
     <div className="Counter">
       <div className="counter-container">
-       
+     
         <h1>Counter App</h1>
-        <div>Count {count}</div>
-         <br/>
+
+       
+        <div>Count: {count}</div> 
+        
+        <br />
+
+     
         <button onClick={increment} className="increment-btn">Increment</button>
         <button onClick={decrement} className="decrement-btn">Decrement</button>
       </div>
